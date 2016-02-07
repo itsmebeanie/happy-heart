ac41bf885de00045881657652d736258c607a9d0
kaitlingu <kaitlin.gu@gmail.com>
2016-02-06 22:53:07 -0500

    integrated styles

--- public/javascripts/viz.js
+++ public/javascripts/viz.js
@@ -23,7 +23,7 @@ var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.close); });
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#d3").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -39,11 +39,11 @@ d3.tsv("data.tsv", type, function(error, data) {
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
-            .attr("x", 10)
-                  .attr("dx", ".71em")
           .append("text")
+                            .attr("dx", ".71em")
+                            .attr("x",width-30)
           .style("text-anchor", "begin")
-      .text("Time");
+      .text("Time (hours)");
 
 
 	
@@ -57,21 +57,95 @@ d3.tsv("data.tsv", type, function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Heartrate()");
+      .text("Heartrate(bpm)");
 
   svg.append("path")
       .datum(data)
       .attr("class", "line")
       .attr("d", line);
-      svg.selectAll(".dot")
+      
+  var g = svg.selectAll(".dot")
             .data(data)
-            .enter().append("circle")
+            .enter().append("g")  
+            
+
+      g.append("circle")
             .attr("class", "dot")
-            .attr("r", 4)
-            .attr('fill-opacity', 0.5)
+            .attr("r", 9)
             .attr("cx", function(d) { return x(d.date); })
             .attr("cy", function(d) { return y(d.close); })
-            .style("fill","red");
+            .attr('fill-opacity', 0.5)
+           .style('fill',function (d) {return d.color;})
+  g.append("text")
+            .attr("class", "comments")
+.style("font-size","13px")
+      .attr("x", function(d) { return x(d.date) + 5 ; })
+        .attr("y", function(d) { return y(d.close) - 10; })
+                    .attr('fill-opacity', 0.2)
+
+    .text(function(d){
+      return d.comment;
+    });
+
+
+      svg.selectAll('circle').on("mouseover", rollover);
+            svg.selectAll('.comments').on("mouseover", displayText);
+
+      svg.selectAll('circle, .comments').on("mouseleave",rollaway);
+
+      //svg.selectAll('text').on("mouseover",removeText);
+      svg.selectAll('circle').on("click", clickedCircle);
+
+      function rollover(){
+        d3.select(this).attr('fill-opacity',1.0);
+
+              }
+
+
+function displayText(){
+        d3.select(this)
+                .style("fill","black")
+                .attr("stroke-width",1.0)
+                .attr("stroke", "black")
+                .attr('fill-opacity',0.8);
+        
+
+          console.log("bleh");
+      }
+
+        function rollaway(){
+          d3.select(this).attr('fill-opacity',0.2)
+        .attr("stroke","none");
+      }
+
+
+
+      
+
+        function clickedCircle(){
+          console.log(this);
+          //openPopup(this.className);
+          
+         
+      }
+
+      d3.select(window).on('resize', resize); 
+
+function resize() {
+  /*console.log('thishappens');
+    // update width
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 500 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+  d3.select('#graph').attr('width',40);
+
+    // reset x range
+    x.range([0, width]);
+
+    // do the actual resize...*/
+}
 
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Track Your Heartrate' });
+  res.render('index', { title: 'Track Your Heartrate', mood: "sad", message: "Your overall emotion was: SAD. Check out this recommendation." });
 });
 
 module.exports = router;
