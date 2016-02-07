526d0ae2d4c39272ccc78b825b448f4fd53da97a
Jared Baribeau <jared.baribeau@gmail.com>
2016-02-07 03:10:54 -0500

    Fix the data viz maybe?

--- public/javascripts/viz.js
+++ public/javascripts/viz.js
@@ -47,7 +47,7 @@ var svg = d3.select("#d3").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv(data, type, function(error, data) {
+d3.tsv("data.tsv", type, function(error, data) {
   if (error) throw error;
 
   x.domain(d3.extent(data, function(d) { return d.date; }));
