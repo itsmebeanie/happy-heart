bf5fbcd2e9f867279417804de7c74ee120c4dae4
Jared Baribeau <jared.baribeau@gmail.com>
2016-02-06 19:41:54 -0500

    API call fixed.

--- routes/appapi.js
+++ routes/appapi.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'app api' });
+  // res.render('index', { title: 'app api' });
+	var obj={};
+	obj.success = true;
+	obj.message="success";
+ 	return  res.json(200, {data:obj});
 });
 
 module.exports = router;
